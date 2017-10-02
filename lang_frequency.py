45567355c1c4efe7fe7196aa52816673987d6f31
Ira <you@example.com>
2017-10-02 23:11:38 +0300

    create task

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,23 @@
+from collections import Counter
+import sys
+import re
 
+words_dict = Counter()
 
 def load_data(filepath):
-    pass
+    file_object = open(filepath, "r")
+    lines = file_object.readlines()
+    for line in lines:
+        match_patterns = re.findall(r'\b\w{2,25}\b', line)
+        for word in match_patterns:
+            words_dict[word] += 1
+    get_most_frequent_words(words_dict)
 
 
-def get_most_frequent_words(text):
-    pass
+def get_most_frequent_words(words_dict):
+    for word in words_dict.most_common(10):
+        print('{} found {} times'.format(word[0], word[1]))
 
 
 if __name__ == '__main__':
-    pass
+    load_data(sys.argv[1])
