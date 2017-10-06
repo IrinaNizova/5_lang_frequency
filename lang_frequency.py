780349da06e2b6e9f7a2a669e6bf1b5d7bbccf72
Ira <you@example.com>
2017-10-06 18:44:05 +0300

    fix bugs

--- lang_frequency.py
+++ lang_frequency.py
@@ -13,15 +13,11 @@ def count_words(text):
     return Counter(match_patterns)
 
 
-def get_most_frequent_words(words_dict):
-    frequent_words_number = 10
-    return words_dict.most_common(frequent_words_number)
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("file_name", help="write name of json file")
     text = load_data(parser.parse_args().file_name)
+    frequent_words_number = 10
     words_dict = count_words(text)
-    for word in get_most_frequent_words(words_dict):
-        print('{} found {} times'.format(word[0], word[1]))
+    for word, counter in words_dict.most_common(frequent_words_number):
+        print('{} found {} times'.format(word, counter))
